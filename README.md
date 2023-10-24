bf45c9eff29640ed21214e40d538f6daac023195
Josh Jeppson <joshua.jeppson@usu.edu>
2023-10-24 14:18:13 -0600

    README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Wayfarer - A proof-of-concept seed-path heuristic
+
+If successful, may be integrated into [STAMINA](https://github.com/fluentverification/stamina-storm) and/or [RAGTIMER](https://github.com/fluentverification/ragtimer)
---
+++ src/counterexample.py
@@ -0,0 +1,61 @@
+from .distance import *
+from .crn import *
+
+import queue
+
+backward_pointers = {}
+counterexamples = []
+
+all_transitions = []
+
+def get_transitions(state, crn : Crn):
+	'''
+Use properties of a VASS to get the enabled transitions of a particular
+	'''
+	# global all_transitions
+	transitions = []
+	for transition in crn.transitions:
+		if transition.enabled(state):
+			transitions.append((transition.rate_finder(state), state))
+	return transitions
+
+def traceback(c_state, tail=[]):
+	'''
+recursive algorithm to get all tracebacks to init state from current state
+	'''
+	# TODO: Dynamic programming version of this
+	global backward_pointers
+	global counterexamples
+	# We can afford a copy of the tail on every call, since traceback is only called from
+	# satisfying states and works BACKWARDS
+	new_tail = tail.copy()
+	new_tail.append(c_state)
+	for state in backward_pointers[c_state]:
+		if not state in backward_pointers:
+			counterexamples.append(new_tail)
+		else:
+			traceback(state, new_tail)
+
+def find_counterexamples(crn, number=1):
+	global backward_pointers
+	boundary = crn.boundary
+	init_state = crn.init_state
+	# According to https://docs.python.org/3/library/queue.html, the priority queue
+	# is, by default, a min queue
+	pq = queue.PriorityQueue()
+	# TODO: according to the same docs, these queues are threadsafe, so
+	# we can eventually multithread this
+	global counterexamples
+	curr_state = None
+	pq.put((vass_priority(init_state), init_state))
+	while (not pq.empty()) and len(counterexamples) < number:
+		curr_state = pq.get()
+		if satisfies(curr_state, boundary):
+			traceback(curr_state)
+		priority = vass_priority(curr_state)
+		for _, next_state in get_transitions(curr_state, crn):
+			if not next_state in backward_pointers:
+				backward_pointers[next_state] = [curr_state]
+			else:
+				backward_pointers[next_state].append(curr_state)
+
