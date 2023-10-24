693746a39aec8119d995b9879ef798acdfd1146f
Josh Jeppson <joshua.jeppson@usu.edu>
2023-10-24 14:17:21 -0600

    more stuff

--- src/crn.py
+++ src/crn.py
@@ -1,18 +1,23 @@
+import numpy as np
+
 class Transition:
-	vector      : np.vector
-	enabled     : lambda
-	rate_finder : lambda
-	def __init__(self, vector : np.vector, enabled : lambda, rate_finder : lambda):
-		self.vector = vector
+	vector      #: np.vector
+	enabled     #: lambda
+	rate_finder #: lambda
+	def __init__(self, vector, enabled, rate_finder):
+		self.vector = np.vector(vector)
 		self.enabled = enabled
 		self.rate_finder = rate_finder
 
 class Crn:
-	def __init__(self, transitions=None : list):
+	def __init__(self, transitions=None : list, boundary : list, init_state):
+		self.boundary = boundary
 		if transitions is None:
 			self.transitions = []
 		else:
 			self.transitions = transitions
+		self.init_state = init_state
 
 	def add_transition(self, transition : Transition):
 		self.transitions.append(transition)
+
---
+++ tests/test1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+from crn import *
+from counterexample import *
+
+crn = Crn([
+	# Transition system
+	Transition(
+		[-10, 0, 0]
+		, lambda state : state[0] > 50
+		, lambda state : 1.7
+		)
+	, Transition(
+		[40, 0, 0]
+		, lambda state : state[0] < 1000
+		, lambda state : 5.7
+		)
+	, Transition(
+		[0, 1, 0]
+		, lambda state : state[1] < 100
+		, lambda state : 3
+		)
+	, Transition(
+		[0, -1, 0]
+		, lambda state : state[1] > 1
+		, lambda state : 0.5
+		)
+	, Transition(
+		[0, 0, 10]
+		, lambda state : state[2] < 100
+		, lambda state : 4
+		)
+	, Transition(
+		[0, 0, -2]
+		, lambda state : state[2] > 2
+		, lambda state : 0.3
+		)
+	]
+	# The satisfying condition
+	, [Bound(650, BoundTypes.GREATER_THAN), Bound(20, BoundTypes.GREATER_THAN), Bound(3, BoundTypes.EQUAL)]
+	# , lambda state : return state[0] > 650 and state[1] > 20 and state[3] == 8
+	# The initial state
+	, np.vector([0, 0, 0])
+)
+
+if __name__=="__main__":
+	find_counterexamples(crn, number=3)
