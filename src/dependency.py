f62dd708f46c3577523b48bbad10c05b84502624
Joshua Jeppson <joshua.jeppson17@gmail.com>
2023-12-07 16:30:47 -0700

    Small changes

--- src/dependency.py
+++ src/dependency.py
@@ -1,26 +1,49 @@
 # TODO: re-implement Landon's dependency graph
 
 class Node:
-	def __init__(self, reaction, level = 0):
+	'''
+	A Node in the dependency graph
+	'''
+	def __init__(self, reaction, level = 0, count = 1):
+		'''
+		A constructor for a node with no successors
+		'''
 		self.reaction = reaction
 		self.children = []
 		self.level = level
+		self.count = count
 
-	def __init__(self, reaction, children, level = 0):
+	def __init__(self, reaction, children, level = 0, count = 1):
+		'''
+		A constructor for a node with successors (children)
+		'''
 		self.reaction = reaction
 		self.children = children
 		self.level = level
+		self.count = count
 
 	def add_child(self, child):
+		'''
+		Appends a successor to the node's children
+		'''
 		self.children.append(child)
 
 class Reaction:
+	'''
+	A reaction (can read in a line from a ragtimer file)
+	'''
 	def __init__(self, name, in_species, out_species):
+		'''
+		Basic constructor if we know species used and produced
+		'''
 		self.name = name
 		self.in_species = in_species
 		self.out_species = out_species
 
 	def __init__(self, line, dep_graph):
+		'''
+		A constructor which parses a RAGTIMER line
+		'''
 		line_elems = line.replace("\n", "").split("\n")
 		self.name = line_elems
 		sep_idx = line.index(">")
@@ -34,13 +57,20 @@ class Reaction:
 				dep_graph.declare_consumer(self, spec)
 
 class DepGraph:
+	'''
+	A basic graph class for the entire dependency graph
+	'''
 	def __init__(self, reactions, species_names, desired_values, init_state):
+		'''
+		Builds the dependency graph
+		'''
 		self.producers = {}
 		self.consumers = {}
 		self.reactions = []
 		self.reaction_levels = []
 		self.species_names = species_names
 		self.root = []
+		# Creates the reactions
 		for reaction in reactions:
 			r = Reaction(reaction, self)
 			self.reactions.append(r)
@@ -48,8 +78,13 @@ class DepGraph:
 		self.graph_root = self.create_graph(desired_values, init_state)
 
 	def create_graph(self, desired_values, state, level = 0):
+		'''
+		Recursive function that creates the graph
+		'''
+		# Decreases the values in the change vector
 		successors = []
 		change = desired_values - state
+		# If c is all zeros, we don't recurse any further
 		for c in change:
 			species = species_names[species_idx]
 			if c > 0:
@@ -57,36 +92,48 @@ class DepGraph:
 				spec_producers = self.producers[species]
 				for producer in spec_producers:
 					new_state = state.copy()
-					new_state[species_idx] += 1
+					new_state[species_idx] = 0
+					count = abs(c)
 					self.declare_reaction_at_level(producer, level)
 					# recurse down until satisfied
 					next_reactions = self.create_graph(desired_values, new_state)
-					successors.append(Node(producer, next_reactions, level + 1))
+					successors.append(Node(producer, next_reactions, level + 1, count))
 			elif c < 0:
 				# We need a consumer reaction
 				spec_consumers = self.consumers[species]
 				for consumer in spec_consumers:
 					new_state = state.copy()
-					new_state[species_idx] -= 1
+					new_state[species_idx] = 0
+					count = abs(c)
 					self.declare_reaction_at_level(consumer, level)
 					next_reactions = self.create_graph(desired_values, new_state)
 					# recurse down until satisfied
-					successors.append(Node(consumer, next_reactions, level + 1))
+					successors.append(Node(consumer, next_reactions, level + 1, count))
 			species_idx += 1
 		return successors
 
 	def declare_reaction_at_level(self, reaction, level);
+		'''
+		Declares the reaction at a specific level in the graph (for easy parsing later without having to
+		traverse the actual graph)
+		'''
 		while len(self.reaction_levels) <= level:
 			self.reaction_levels.append([])
 		self.reaction_levels[level].append(reaction)
 
 	def declare_producer(self, reaction, species):
+		'''
+		Declares a reaction as a producer (called by the reaction's constructor)
+		'''
 		if not species in self.producers:
 			self.producers[species] = [reaction]
 		else:
 			self.producers[species].append(reaction)
 
 	def declare_consumer(self, reaction, species):
+		'''
+		Declares a reaction as a consumer (called by the reaction's constructor)
+		'''
 		if not species in self.consumers:
 			self.consumers[species] = [reaction]
 		else:
