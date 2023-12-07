eb950f4cada07537f5cf7c996b4965161cf53f3f
Joshua Jeppson <joshua.jeppson17@gmail.com>
2023-12-07 16:16:01 -0700

    First dependency graph attempt

--- src/dependency.py
+++ src/dependency.py
@@ -1,13 +1,15 @@
 # TODO: re-implement Landon's dependency graph
 
 class Node:
-	def __init__(self, reaction):
+	def __init__(self, reaction, level = 0):
 		self.reaction = reaction
 		self.children = []
+		self.level = level
 
-	def __init__(self, reaction, children):
+	def __init__(self, reaction, children, level = 0):
 		self.reaction = reaction
 		self.children = children
+		self.level = level
 
 	def add_child(self, child):
 		self.children.append(child)
@@ -21,9 +23,9 @@ class Reaction:
 	def __init__(self, line, dep_graph):
 		line_elems = line.replace("\n", "").split("\n")
 		self.name = line_elems
-		sep_idx = transition_info.index(">")
-		self.in_species = transition_info[1:sep_idx]
-		self.out_species = transition_info[sep_idx + 1:len(transition_info) - 1]
+		sep_idx = line.index(">")
+		self.in_species = line[1:sep_idx]
+		self.out_species = line[sep_idx + 1:len(line) - 1]
 		for spec in self.out_species:
 			if not spec in self.in_species:
 				dep_graph.declare_producer(self, spec)
@@ -33,21 +35,50 @@ class Reaction:
 
 class DepGraph:
 	def __init__(self, reactions, species_names, desired_values, init_state):
-		change = desired_values - init_state
 		self.producers = {}
 		self.consumers = {}
 		self.reactions = []
+		self.reaction_levels = []
+		self.species_names = species_names
 		self.root = []
 		for reaction in reactions:
 			r = Reaction(reaction, self)
 			self.reactions.append(r)
+		species_idx = 0
+		self.graph_root = self.create_graph(desired_values, init_state)
+
+	def create_graph(self, desired_values, state, level = 0):
+		successors = []
+		change = desired_values - state
 		for c in change:
+			species = species_names[species_idx]
 			if c > 0:
-				# TODO: recurse down until satisfied
-				pass
+				# We need a producer reaction
+				spec_producers = self.producers[species]
+				for producer in spec_producers:
+					new_state = state.copy()
+					new_state[species_idx] += 1
+					self.declare_reaction_at_level(producer, level)
+					# recurse down until satisfied
+					next_reactions = self.create_graph(desired_values, new_state)
+					successors.append(Node(producer, next_reactions, level + 1))
 			elif c < 0:
-				# TODO: also recurse down
-				pass
+				# We need a consumer reaction
+				spec_consumers = self.consumers[species]
+				for consumer in spec_consumers:
+					new_state = state.copy()
+					new_state[species_idx] -= 1
+					self.declare_reaction_at_level(consumer, level)
+					next_reactions = self.create_graph(desired_values, new_state)
+					# recurse down until satisfied
+					successors.append(Node(consumer, next_reactions, level + 1))
+			species_idx += 1
+		return successors
+
+	def declare_reaction_at_level(self, reaction, level);
+		while len(self.reaction_levels) <= level:
+			self.reaction_levels.append([])
+		self.reaction_levels[level].append(reaction)
 
 	def declare_producer(self, reaction, species):
 		if not species in self.producers:
--- src/subspace.py
+++ src/subspace.py
@@ -7,7 +7,7 @@ import numpy as np
 	# import cupy as np
 
 # if VERIFY:
-from nagini_contracts.contracts import *
+# from nagini_contracts.contracts import *
 from typing import List
 
 from distance import vass_distance
@@ -38,9 +38,9 @@ class Subspace:
 	# @Pure
 	def contains(self, other : Subspace, test_vec : np.matrix) -> bool:
 		# Check if contains
-		Requires(type(State.init) == np.matrix)
-		Requires(len(test_vec) == len(Subspace.mask))
-		Ensures(Implies(Result(), self.dist(test_vec) >= other.dist(test_vec)))
+		# Requires(type(State.init) == np.matrix)
+		# Requires(len(test_vec) == len(Subspace.mask))
+		# Ensures(Implies(Result(), self.dist(test_vec) >= other.dist(test_vec)))
 		if self.rank < other.rank:
 			return False
 		return np.linalg.matrix_rank(np.block([self.P, other.P])) == self.rank
@@ -51,14 +51,14 @@ class Subspace:
 		Computes the norm of a vector, only accounting for species in the CRN that
 		aren't "Don't care" (value -1)
 		'''
-		Requires(len(vec) == len(Subspace.mask))
-		Ensures(Result() >= 0.0)
+		# Requires(len(vec) == len(Subspace.mask))
+		# Ensures(Result() >= 0.0)
 		return float(np.linalg.norm(np.multiply(vec, Subspace.mask)))
 
-	@Pure
+	# @Pure
 	def dist(self, vec) -> float:
-		Requires(len(vec) == len(Subspace.mask))
-		Ensures(Result() >= 0.0)
+		# Requires(len(vec) == len(Subspace.mask))
+		# Ensures(Result() >= 0.0)
 		return self.norm(self.P * vec - vec)
 
 
@@ -69,6 +69,10 @@ class State:
 	# the species we don't
 
 	init : np.matrix = None
+	@staticmethod
+	def initialize_static_vars(filename):
+		pass # TODO
+
 	def __init__(self, vec : np.matrix):
 		'''
 		Constructor for a new State element. Members within the State class:
@@ -81,12 +85,12 @@ class State:
 		4. epsilon (type list(int)) : The list of nonzero subspace distances, starting
 		with the largest and working towards the smallest
 		'''
-		Requires(type(State.init) == np.matrix)
-		Requires(type(State.target) == np.matrix)
-		Requires(type(State.subspaces) == List[Subspace])
-		# Requires(Forall(int, lambda i : Implies(i > 0 and i < len(State.subspaces), State.subspaces[i].contains(State.subspaces[i - 1], State.init))))
-		Ensures(self.order >= -1)
-		Ensures(len(self.epsilon) == len(State.subspaces) + 1)
+		# Requires(type(State.init) == np.matrix)
+		# Requires(type(State.target) == np.matrix)
+		# Requires(type(State.subspaces) == List[Subspace])
+		# # Requires(Forall(int, lambda i : Implies(i > 0 and i < len(State.subspaces), State.subspaces[i].contains(State.subspaces[i - 1], State.init))))
+		# Ensures(self.order >= -1)
+		# Ensures(len(self.epsilon) == len(State.subspaces) + 1)
 		self.vec = vec
 		self.adj = vec - State.init
 		self.order : int = 0
@@ -96,12 +100,12 @@ class State:
 		'''
 		Computes the order and epsilon vector of the state
 		'''
-		Requires(type(State.init) == np.matrix)
-		Requires(type(State.target) == np.matrix)
-		Ensures(type(self.order) == int and self.order >= -1)
-		Ensures(type(self.epsilon == list[float]))
-		Ensures(len(self.epsilon) >= 1)
-		Ensures(Forall(int, lambda i : (Implies(i > 0 and i < len(State.subspaces), self.epsilon[i] >= self.epsilon[i - 1]))))
+		# Requires(type(State.init) == np.matrix)
+		# Requires(type(State.target) == np.matrix)
+		# Ensures(type(self.order) == int and self.order >= -1)
+		# Ensures(type(self.epsilon == list[float]))
+		# Ensures(len(self.epsilon) >= 1)
+		# Ensures(Forall(int, lambda i : (Implies(i > 0 and i < len(State.subspaces), self.epsilon[i] >= self.epsilon[i - 1]))))
 		dist_to_target = State.subspaces[0].norm(self.vec - State.target)
 		if dist_to_target == 0.0:
 			self.epsilon : list = [0.0]
@@ -122,13 +126,13 @@ class State:
 		Only returns the successors using the vectors in the dependency graph
 		that get us closer to the target.
 		'''
-		Requires(type(State.init) == np.matrix)
-		Requires(type(State.target) == np.matrix)
-		Ensures(type(Result()) == tuple)
-		Ensures(len(Result()) == 2)
-		Ensures(type(Result()[0]) == list)
-		Ensures(type(Result()[1]) == float)
-		Ensures(Result()[1] >= 0.0)
+		# Requires(type(State.init) == np.matrix)
+		# Requires(type(State.target) == np.matrix)
+		# Ensures(type(Result()) == tuple)
+		# Ensures(len(Result()) == 2)
+		# Ensures(type(Result()[0]) == list)
+		# Ensures(type(Result()[1]) == float)
+		# Ensures(Result()[1] >= 0.0)
 		succ = []
 		total_outgoing_rate = 0.0
 		subspace = State.subspaces[len(State.subspaces) - (self.order + 1)]
@@ -153,44 +157,44 @@ class State:
 		return succ, total_outgoing_rate
 
 	# Comparators. ONLY COMPARES THE ORDER AND THE LOWEST VALUE FOR EPSILON
-	@Pure
+	# @Pure
 	def __gt__(self, other):
-		Requires(len(self.epsilon) == len(other.epsilon))
-		Requires(len(self.epsilon) > 0)
+		# Requires(len(self.epsilon) == len(other.epsilon))
+		# Requires(len(self.epsilon) > 0)
 		return self.order > other.order or self.epsilon[0] > other.epsilon[0]
 
-	@Pure
+	# @Pure
 	def __lt__(self, other):
-		Requires(len(self.epsilon) == len(other.epsilon))
-		Requires(len(self.epsilon) > 0)
+		# Requires(len(self.epsilon) == len(other.epsilon))
+		# Requires(len(self.epsilon) > 0)
 		return self.order < other.order or self.epsilon[0] < other.epsilon[0]
 
-	@Pure
+	# @Pure
 	def __le__(self, other):
-		Requires(len(self.epsilon) == len(other.epsilon))
-		Requires(len(self.epsilon) > 0)
+		# Requires(len(self.epsilon) == len(other.epsilon))
+		# Requires(len(self.epsilon) > 0)
 		return not self > other
 
-	@Pure
+	# @Pure
 	def __ge__(self, other):
-		Requires(len(self.epsilon) == len(other.epsilon))
-		Requires(len(self.epsilon) > 0)
+		# Requires(len(self.epsilon) == len(other.epsilon))
+		# Requires(len(self.epsilon) > 0)
 		return not self < other
 
-	@Pure
+	# @Pure
 	def __eq__(self, other):
 		'''
 		Weak equal means are we equally close to the next subspace.
 		Operator overloading done here because of priority queue, which
 		possibly may use `=` operator.
 		'''
-		Requires(len(self.epsilon) == len(other.epsilon))
-		Requires(len(self.epsilon) > 0)
+		# Requires(len(self.epsilon) == len(other.epsilon))
+		# Requires(len(self.epsilon) > 0)
 		return self.order == other.order or self.epsilon[0] == other.epsilon[0]
 
 	# Strong equality means we are the same state
-	@Pure
+	# @Pure
 	def strong_equal(self, other):
-		Requires(len(self.vec) == len(other.vec))
+		# Requires(len(self.vec) == len(other.vec))
 		return self.vec == other.vec
 
