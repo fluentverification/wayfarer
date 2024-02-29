20a1e6b93598c785d2cc8dff2bd9df4aeddd60aa
Josh Jeppson <joshua.jeppson@usu.edu>
2024-02-29 13:05:20 -0700

    I think this will work to get minimal scaling factor

--- src/cycle.py
+++ src/cycle.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+from crn import *
+from subspace import *
+
+from fractions import Fraction
+
 class Cycle:
 	def __init__(self, ordered_reactions : list):
 		'''
@@ -18,7 +23,7 @@ class Cycle:
 			sum += r.vec_as_mat
 		assert(np.all(np.is_close(sum, 0)))
 
-	def apply_cycle(self, state : np.vector) -> list:
+	def apply_cycle(self, state : np.matrix) -> list:
 		s = state
 		new_states = []
 		for r in self.__ordered_reactions:
@@ -33,3 +38,62 @@ def get_commutable_transitions(crn : Crn, s0 : Subspace, ss : Subspace) -> list:
 		if np.all(np.is_close(s0.P * t.vec_as_mat, 0)).all() and np.all(np.isclose(ss.P * t.vec_as_mat, t.vec_as_mat)):
 			transitions.append(t)
 	return t
+
+primes = [2]
+
+def is_prime(i : int) -> bool:
+	for j in range(2, i):
+		if i % j == 0:
+			return False
+	return True
+
+def init_primes_to(n : int):
+	last_prime = primes[len(primes) - 1]
+	for i in range(last_prime, n):
+		if is_prime(i + 1):
+			primes.append(i + 1)
+
+def get_prime_factors(n : int) -> list:
+	factors = []
+	init_primes_to(n)
+	for p in primes:
+		if p == n:
+			return [(p, 1)]
+		elif p > n:
+			break
+		exponent : int = 0
+		while n % p == 0:
+			exponent += 1
+			n /= p
+		if exponent > 0:
+			factors.append((p, exponent))
+	return factors
+
+def get_fraction(f : Fraction) -> tuple:
+	'''
+Requires type Fraction
+	
+Old: ~~In order to get past floating point weirdness, we have to put the float
+into a string first.~~
+	'''
+	assert(type(f) == Fraction)
+	return f.as_integer_ratio()
+	# return Fraction(str(f)).as_integer_ratio()
+
+def minimal_scaling_factor(vec : np.matrix) -> int:
+	'''
+This does not work if the data type of the matrix is `float`
+	'''
+	primes = {}
+	for elem in vec:
+		n, d = get_fraction(elem[0, 0])
+		print(f"{n} / {d}")
+		factors = get_prime_factors(d)
+		for p, e in factors:
+			primes[p] = max(primes[p], e) if p in primes else e
+	scale_factor = 1
+	for prime, exponent in primes.items():
+		scale_factor *= prime ** exponent
+	return scale_factor
+	
+
