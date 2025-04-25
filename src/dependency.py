c8a012e973380341ab9fcf59aebca1a704fec739
Josh Jeppson <joshua.jeppson@usu.edu>
2025-04-25 11:06:59 -0600

    fix bug on single-state solution models

--- src/dependency.py
+++ src/dependency.py
@@ -164,7 +164,7 @@ class DepGraph:
 		sa = self.particular_solution - self.init_state
 		Mpsivecs = self.sat_basis.copy()
 		if len(Mpsivecs) == 0:
-			return sa
+			return np.zeros(np.size(sa))
 		# print(Avecs)
 		Mpsi = np.column_stack(Mpsivecs)
 		# print(A)
